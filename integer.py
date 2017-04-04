a6ffb9a1523e8ef995fbc4ea1a8227bf4e53a7ed
Vasilev Maxim <vsmaxim8@gmail.com>
2017-04-05 00:30:00 +0300

    Added comments for integer and rational.

--- integer.py
+++ integer.py
@@ -4,35 +4,47 @@ from naturals import Natural
 class Integer(Natural):
     def __init__(self, digits):
         '''Конструктор целого числа, Васильев Максим'''
+        # Целое число хранится в перевёрнутом виде в .digits
+        # Знак целого числа хранится в bool в .negative
+        # True - отрицательное, False - неотрицательное
         self.negative = digits[0] == '-'
         if self.negative:
             digits = digits[1:]
         self.digits = [int(i) for i in reversed(digits)]
 
     def __str__(self):
-        '''Представление целого числа в строке, Васильев Максим'''
+        '''Строковое представление integer, Васильев Максим'''
         s = ''
+        # Если ноль - возвращаем строку с нулём
         if self.isZero():
             return '0'
+        # Если отрицательное, то добавляем '-'
         if self.negative:
             s = '-'
+        # Возвращаем <object String>
         return s + ''.join(str(i) for i in reversed(self.digits))
 
     def __neg__(self):
         '''MUL_ZM_Z Умножение целого на -1, Васильев Максим'''
+        # Делаем копию
         a = Integer(str(self))
+        # Меняем флаг negative на противоположный
         a.negative = not a.negative
+        # Возвращаем <object Integer>
         return a
 
     def abs(self):
         '''ABS_Z_N Абсолютная величина числа, Васильев Максим'''
         res = Integer('0')
         res.digits = self.digits
+        # Делаем число положительным
         res.negative = False
+        # Возвращаем <object Integer>
         return res
 
     def isZero(self):
         '''Проверка целого числа на ноль, Васильев Максим'''
+        # Алгоритм, аналогичный реализованному для натуральных
         for i in range(len(self.digits)):
             if self.digits[i]:
                 return False
@@ -42,13 +54,18 @@ class Integer(Natural):
         '''Возведение целого числа в степень, Васильев Максим'''
         a = Natural(str(self.abs()))
         b = Natural(str(oth.abs()))
+        # Результат - возведение натурального a в b
         res = Integer(str(a ** b))
+        # Отрицательность получается из соображений четности степени
         if self.negative and (b % Natural('2') == Natural('1')):
             res.negative = self.negative
+        # Возвращаем <object Integer>
         return res
 
     def positivity(self):
         '''POZ_Z_D Определение положительности числа, Васильев Максим'''
+        # Возвращает 0 если число равно нулю, если отрицательное -1
+        # и если положительное - 1
         if self.isZero():
             return 0
         elif self.negative:
@@ -57,74 +74,103 @@ class Integer(Natural):
             return 1
 
     def __le__(self, oth):
+        # Перегрузка оператора < для целых чисел
+        # если отрицательность чисел разная - возвращает
+        # значение отрицательности первого числа, что в
+        # точности совпадает с результатом оператора <,
+        # можете проверить и удостовериться в этом
         if self.negative != oth.negative:
             return self.negative
         else:
             if self.negative:
-                return not self.abs().ton() < oth.abs().ton()
+                return not self.ton() < oth.ton()
             else:
-                return self.abs().ton() < oth.abs().ton()
+                return self.ton() < oth.ton()
 
     def ton(self):
         '''Преобразование Integer->Natural, Васильев Максим'''
+        # Возвращаем <object Natural>
         return Natural(str(self.abs()))
 
     def __add__(self, oth):
         '''Сложение двух целых чисел, Васильев Максим'''
+        # Инициализируем по аналогии с предыдущими модулями
+        # a и b натуральные значения целых
         result = Integer('0')
         a = self.ton()
         b = oth.ton()
+        # Если числа одного знака, то просто складываем со
+        # взглядом на знак
         if self.negative == oth.negative:
             result = Integer(str(a + b))
             result.negative = self.negative
         else:
+            # Если числа разных знаков, то вычитаем и отдаем знак большего
+            # из двух чисел
             result = Integer(str(a - b))
             if (a < b):
                 result.negative = oth.negative
             else:
                 result.negative = self.negative
+        # Возвращаем <object Integer>
         return result
 
     def __sub__(self, oth):
         '''Вычитание двух целых чисел, Васильев Максим'''
+        # Преобразуем вычитание к сложению с унарным минусом
+        # возвращаем <object Integer>
         return self + -oth
 
     def __mul__(self, oth):
         '''Умножение двух целых чисел, Васильев Максим'''
         a = self.ton()
         b = oth.ton()
+        # Результат - умножение натуральных
         result = Integer(str(a * b))
+        # Знак результата соответствует данному булеву выражению
         result.negative = self.negative != oth.negative
+        # Возвращаем <object Integer>
         return result
 
     def div(self, oth):
         '''Деление, возвращающее остаток и целую часть, Васильев Максим'''
+        # Деление целых аналогично умножению целых, также пользуемся
+        # Уже готовым модулем для натуральных чисел
         a = self.ton()
         b = oth.ton()
         res = a.div(b)
         for i in range(len(res)):
             res[i] = Integer(str(res[i]))
             res[i].negative = self.negative != oth.negative
+        # Возвращаем <object Integer>
         return res
 
     def __mod__(self, oth):
         '''Перегрузка операции %, остаток от деления'''
         res = self.div(oth)[1]
+        # Если остаток отрицательный - добавляем к нему один раз модуль
         if res.negative:
             res = res + oth.abs()
+        # Возвращаем <object Integer>
         return res
 
     def __div__(self, oth):
         '''Перегрузка операции //, целая часть от деления'''
+        # Пользуемся результатом метода .div()
+        # Возвращаем <object Integer>
         return self.div(oth)[0]
 
     def ferma(self):
         '''Метод ферма для нечетных чисел, и четных имеющих в разложении 2
         более чем в 1 степени, Васильев Максим'''
+        # Задаем начальные значения rx, ry и rxy исходя из примерного значения
+        # корня из числа
         a = Integer(str(self.ton().sqrt()))
         rx = Integer('2') * a + Integer('1')
         ry = Integer('1')
         rxy = a ** Integer('2') - self
+        # Пока выражение от x, y не равно нулю, увеличиваем или уменьшаем в
+        # зависимости от ситуации rx и ry и добавляем или вычитаем их же
         while not rxy.isZero():
             if rxy <= Integer('0'):
                 rxy = rxy + rx
@@ -132,11 +178,5 @@ class Integer(Natural):
             else:
                 rxy = rxy - ry
                 ry = ry + Integer('2')
+        # Возвращаем результат разложения в виде a * b где a и b - делители
         return str((rx - ry) // Integer('2')) + '*' + str((rx + ry) // Integer('2') - Integer('1'))
-
-
-# Для тестов
-if __name__ == '__main__':
-    a = Integer('-7')
-    b = Integer('-5')
-    print(a - b)
--- rational.py
+++ rational.py
@@ -18,70 +18,97 @@ def contForm(stri):
 class Rational:
     def __init__(self, str):
         '''Конструктор рационального числа, Васильев Максим'''
+        # self.numer - хранит числитель (numerator), self.denom
+        # хранит знаменатель (denominant), числа вводятся в виде
+        # num/den
         a = str.split('/')
         self.numer = Integer(a[0])
         self.denom = Natural(a[1])
 
     def __str__(self):
         '''Строковое представление рационального числа, Васильев Максим'''
+        # Возвращает строку вида 'num/den'
         return '{}/{}'.format(self.numer, self.denom)
 
     def __neg__(self):
         '''Умножение дроби на -1, Васильев Максим'''
+        # Меняем знак на противоположный у числителя
         a = Rational(str(self))
         a.numer = -a.numer
+        # Возвращаем <object Rational>
         return a
 
     def __gt__(self, oth):
         '''Сравнение двух дробей, Васильев Максим'''
+        # Смотрим на знак разности двух дробей, если
+        # отрицательный, то дробь больше, иначе меньше
         return (oth - self).numer.negative
 
     def isZero(self):
         '''Проверка дроби на ноль, Васильев Максим'''
+        # Проверяем знаменатель на ноль, уже имеющимся методом
         return self.numer.isZero()
 
     def __add__(self, oth):
         '''Сложение рациональных чисел, Гусева Екатерина'''
         a = Rational(str(self))
         b = Rational(str(oth))
+        # n_den - НОК знаменателей
         n_den = a.denom.lcm(b.denom)
-        a_k = n_den // a.denom
         # Коэффицент на который домножаем первый числитель
-        b_k = n_den // b.denom
+        a_k = n_den // a.denom
         # Коэффициент на который домножжаем второй числитель
+        b_k = n_den // b.denom
+        # Умножаем коэффициенты и числители, знаменатель - НОК
         n_num = a.numer * Integer(str(a_k)) + b.numer * Integer(str(b_k))
+        # Возвращаем <object Rational>
         return Rational(str(n_num) + '/' + str(n_den))
 
     def __sub__(self, oth):
         '''Вычитание рациональных чисел, Васильев Максим'''
+        # Аналогично с целыми
+        # Возращаем <object Rational>
         return self + -oth
 
     def __mul__(self, oth):
         '''Умножение рациональных чисел, Гусева Екатерина'''
+        # Умножаем числители и знаменатели и сокращаем их
+        # Возвращаем <object Rational>
         return Rational(str(self.numer * oth.numer) + '/' + str(self.denom * oth.denom)).red()
 
     def isInteger(self):
         '''Проверка, является ли дробь - целым, Васильев Максим'''
+        # Проверяем делится ли числитель на знаменатель нацело,
+        # остаток от деления - 0
+        # Возвращаем <object Bool>
         return (self.numer.ton() % self.denom).isZero()
 
     def red(self):
         '''Сокращение дроби, Гусева Екатерина'''
         num = self.numer.ton()
         den = self.denom
+        # Находим НОД числителя и знаменателя
         gcd = num.gcd(den)
+        # Делим числитель и знаменатель на НОД
         self.numer.digits = (num // gcd).digits
         self.denom = den // gcd
+        # Возвращаем <object Rational>
         return self
 
     def turn(self):
         '''Получение вида 1/x - для дроби, Васильев Максим'''
+        # Числитель становится знаменателем
+        # Знаменатель числителем
         n_den = Natural(str(self.numer.abs()))
         n_num = Integer(str(self.denom))
         n_num.negative = self.numer.negative
+        # Возвращаем <object Rational>
         return Rational(str(n_num) + '/' + str(n_den))
 
     def __truediv__(self, oth):
         '''Деление рациональных, Васильев Максим'''
+        # Сводим деление к умножению на обратное
+        # Возвращаем <object Rational>
         return self * oth.turn()
 
     def toContinued(self):
@@ -89,6 +116,9 @@ class Rational:
         a = Natural(str(self.numer.abs()))
         b = Natural(str(self.denom))
         res = ''
+        # Применяем алгоритм Евклида к дробям, записываем
+        # в res строку вида '1 2 3 4 5' где цифры - остатки
+        # от деления
         while not a.isZero() and not b.isZero():
             if a > b:
                 res += str(a // b) + ' '
@@ -96,15 +126,15 @@ class Rational:
             else:
                 res += str(b // a) + ' '
                 b = b % a
+        # Вызываем функцию которая возвращает красивое представление
+        # непрерывной дроби
+        # Возвращает <object String>
         return contForm(res)
 
     def toi(self):
         '''Преобразование в целое рационального, Васильев Максим'''
+        self.red()
+        # Если знаменатель после сокращения равен 1 - то число целое
         if self.denom == Natural('1'):
+            # Возвращаем <object Integer>
             return Integer(str(self.numer))
-
-
-if __name__ == '__main__':
-    a = Rational('599/300')
-    b = Rational('1/4')
-    print(a.toContinued())
